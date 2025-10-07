82625277864101d43bfab2973e69d4950fc4475a
Shreyas <shreyasnsingh@gmail.com>
2025-10-07 00:09:14 -0400

    window setup

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Split the bill")
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+root.geometry(f"{width}x{height}")
+
+root.mainloop()
